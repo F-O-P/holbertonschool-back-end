7ab23af81b436f1091efa5dacb3563058505026f
tayler.coon@gmail.com <tayler.coon@gmail.com>
2023-07-21 14:56:20 -0700

    task 0 - partially functional

---
+++ api/0-gather_data_from_an_api.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+import requests
+import sys
+
+''' This script will gather data from an employee ID and returns
+    information about his/her list progress '''
+
+def get_employee_name(employee_id):
+    ''' This function will return the name of the employee '''
+    url = "{}/{}".format(base_url, employee_id)
+    response = requests.get(url)
+    return response.json().get("name")
+
+def get_assigned_tasks(employee_id):
+    ''' This function will return the number of all tasks
+        assigned to that the employee '''
+    url = "{}/{}/todos".format(base_url, employee_id)
+    response = requests.get(url)
+    return len(response.json())
+
+def get_completed_tasks(employee_id):
+    ''' This function will return the number of tasks
+        that the employee has completed '''
+    finished_tasks = 0
+    url = "{}/{}/todos".format(base_url, employee_id)
+    response = requests.get(url)
+    for task in response.json():
+        if task.get("completed"):
+            finished_tasks += 1
+    return finished_tasks
+
+def print_employee_status(employee_name, completed_tasks, assigned_tasks):
+    ''' This function will return the information about the employee'''
+    print("Employee {} is done with tasks({}/{}):".format(employee_name, completed_tasks, assigned_tasks))
+
+if __name__ == "__main__":
+    employee_id = sys.argv[1]
+    base_url = "https://jsonplaceholder.typicode.com/users"
+    employee_name = get_employee_name(employee_id)
+    assigned_tasks = get_assigned_tasks(employee_id)
+    completed_tasks = get_completed_tasks(employee_id)
+    print_employee_status(employee_name, completed_tasks, assigned_tasks)
