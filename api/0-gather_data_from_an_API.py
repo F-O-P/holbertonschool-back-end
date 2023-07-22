f73a663634b2d172a07788a5c443f508ba185993
tayler.coon@gmail.com <tayler.coon@gmail.com>
2023-07-22 10:59:37 -0700

    eat shit API!

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -20,17 +20,19 @@ def get_assigned_tasks(employee_id):
 def get_completed_tasks(employee_id):
     ''' This function will return the number of tasks
         that the employee has completed '''
-    finished_tasks = 0
+    finished_tasks = []
     url = "{}/{}/todos".format(base_url, employee_id)
     response = requests.get(url)
     for task in response.json():
         if task.get("completed"):
-            finished_tasks += 1
+            finished_tasks.append(task.get("title"))
     return finished_tasks
 
 def print_employee_status(employee_name, completed_tasks, assigned_tasks):
     ''' This function will return the information about the employee'''
-    print("Employee {} is done with tasks({}/{}):".format(employee_name, completed_tasks, assigned_tasks))
+    print("Employee {} is done with tasks({}/{}):".format(employee_name, len(completed_tasks), assigned_tasks))
+    for task in completed_tasks:
+        print("\t {}".format(task))
 
 if __name__ == "__main__":
     employee_id = sys.argv[1]
